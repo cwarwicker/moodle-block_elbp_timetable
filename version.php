0dc509e68e5c01e0e14e3d8238405b5d1f47674e
Conn Warwicker <conn@cmrwarwicker.com>
2020-03-19 13:52:09 +0000

    Finalise codebase for release.

--- version.php
+++ version.php
@@ -2,14 +2,17 @@
 
 /**
  * Version for Timetable
- * 
+ *
  * @copyright 2012 Bedford College
  * @package Bedford College Electronic Learning Blue Print (ELBP)
  * @version 1.0
  * @author Conn Warwicker <cwarwicker@bedford.ac.uk> <conn@cmrwarwicker.com>
- * 
+ *
  */
 
-$plugin->version = 2018061000;
-$plugin->requires = 2014111006;
+$plugin->version = 2020031900;
+$plugin->requires = 2017111300;
 $plugin->component = 'block_elbp_timetable';
+$plugin->dependencies = array(
+    'block_elbp' => 2020031900
+);
